e82a2391ddbbe931758967843fde74a087297cf8
LarionovaVika <150127644+LarionovaVika@users.noreply.github.com>
2024-04-15 22:55:33 +0300

    Update 1д Игра жизнь.py

--- 1д Игра жизнь.py
+++ 1д Игра жизнь.py
@@ -3,10 +3,16 @@ d = input() # мир
 c = int(input()) # количество поколений
 b = [] # мир для изменений
 e = [] # получившийся мир
+f = 0
 for i in d.split('1'):
     b.append([i])
     e.append([i])
 for u in range(c):
+    for q in e:
+        if q[0] == '*':
+            f = 1
+    if f == 0:
+        break
     for y in range(a):
         if y == 0:
             if b[y][0] == '*':
@@ -30,7 +36,6 @@ for u in range(c):
                     e[y][0] = '*'
                 elif b[y - 1][0] == ' ' and b[0][0] == '*':
                     e[y][0] = '*'
-                
         else:
             if b[y][0] == '*':
                 if b[y - 1][0] == '*' and b[y + 1][0] == '*':
@@ -43,7 +48,8 @@ for u in range(c):
                 elif b[y - 1][0] == ' ' and b[y + 1][0] == '*':
                     e[y][0] = '*'
     b = []
+    f = 0
     for t in e:
         for r in t:
-            b.append([])
+            b.append([r])
     print(e)
