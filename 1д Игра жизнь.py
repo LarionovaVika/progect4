9cf3445f833a6e7e1c306e2fbc72a28dc6d901b1
LarionovaVika <150127644+LarionovaVika@users.noreply.github.com>
2024-04-15 22:01:54 +0300

    Update 1д Игра жизнь.py

--- 1д Игра жизнь.py
+++ 1д Игра жизнь.py
@@ -1,41 +1,47 @@
 a = int(input()) # длина мира
 d = input() # мир
 c = int(input()) # количество поколений
-b = []
+b = [] # мир для изменений
+e = [] # получившийся мир
 for i in d.split('1'):
     b.append([i])
+    e.append([i])
 for u in range(c):
     for y in range(a - 1):
         if y == 0:
-            if b[y] == '*':
-                if b[y + 1] == '*' and b[a - 1] == '*':
-                    b[y] = ' '
-                elif b[y + 1] == ' ' and b[a - 1] == ' ':
-                    b[y] = ' '
+            if b[y][0] == '*':
+                if b[y + 1][0] == '*' and b[a - 1][0] == '*':
+                    e[y][0] = ' '
+                elif b[y + 1][0] == ' ' and b[a - 1][0] == ' ':
+                    e[y][0] = ' '
             else:
-                if b[y + 1] == '*' and b[a - 1] == ' ':
-                    b[y] = '*'
-                elif b[y + 1] == ' ' and b[a - 1] == '*':
-                    b[y] = '*'
+                if b[y + 1][0] == '*' and b[a - 1][0] == ' ':
+                    e[y][0] = '*'
+                elif b[y + 1][0] == ' ' and b[a - 1][0] == '*':
+                    e[y][0] = '*'
         elif y == a - 1:
-            if b[y] == '*':
-                if b[y - 1] == '*' and b[0] == '*':
-                    b[y] = ' '
-                elif b[y - 1] == ' ' and b[0] == ' ':
-                    b[y] = ' '
+            if b[y][0] == '*':
+                if b[y - 1][0] == '*' and b[0][0] == '*':
+                    e[y][0] = ' '
+                elif b[y - 1][0] == ' ' and b[0][0] == ' ':
+                    e[y][0] = ' '
             else:
-                if b[y - 1] == '*' and b[0] == ' ':
-                    b[y] = '*'
-                elif b[y - 1] == ' ' and b[0] == '*':
-                    b[y] = '*'
+                if b[y - 1][0] == '*' and b[0][0] == ' ':
+                    e[y][0] = '*'
+                elif b[y - 1][0] == ' ' and b[0][0] == '*':
+                    e[y][0] = '*'
         else:
-            if b[y] == '*':
-                if b[y - 1] == '*' and b[y + 1] == '*':
-                    b[y] = ' '
-                elif b[y - 1] == ' ' and b[y + 1] == ' ':
-                    b[y] = ' '
+            if b[y][0] == '*':
+                if b[y - 1][0] == '*' and b[y + 1][0] == '*':
+                    e[y][0] = ' '
+                elif b[y - 1][0] == ' ' and b[y + 1][0] == ' ':
+                    e[y][0] = ' '
             else:
-                if b[y - 1] == '*' and b[y + 1] == ' ':
-                    b[y] = '*'
-                elif b[y - 1] == ' ' and b[y + 1] == '*':
-                    b[y] = '*'
+                if b[y - 1][0] == '*' and b[y + 1][0] == ' ':
+                    e[y][0] = '*'
+                elif b[y - 1][0] == ' ' and b[y + 1][0] == '*':
+                    e[y][0] = '*'
+    b = []
+    for t in e:
+        b.append(t)
+    print(e)
