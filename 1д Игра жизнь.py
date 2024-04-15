b94b7e23666a6acf2a6fc29e4d437f919e18df22
LarionovaVika <150127644+LarionovaVika@users.noreply.github.com>
2024-04-15 19:30:17 +0300

    Update 1д Игра жизнь.py

--- 1д Игра жизнь.py
+++ 1д Игра жизнь.py
@@ -1,7 +1,11 @@
-a = int(input())
-d = input()
-c = int(input())
+a = int(input()) # длина мира
+d = input() # мир
+c = int(input()) # количество поколений
 b = []
-for u in d.split('1'):
-    b.append([u])
-for i in range(c):
+for i in d.split('1'):
+    b.append([i])
+for u in range(c):
+    for y in range(a - 1):
+        if y == 0:
+            if b[y] == '*':
+                if b[y + 1] == '*' and b[a - 1] == '*':
